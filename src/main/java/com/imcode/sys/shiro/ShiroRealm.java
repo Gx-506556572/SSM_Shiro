ccd49bc2fdd9f6fa23c5413b615fb8dccf62a764
G'x <506556572@qq.com>
2020-06-19 15:22:35 +0800

    Shiro

---
+++ src/main/java/com/imcode/sys/shiro/ShiroRealm.java
@@ -0,0 +1,50 @@
+package com.imcode.sys.shiro;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ShiroRealm extends AuthorizingRealm  {
+
+//登录认证
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken token=(UsernamePasswordToken)authenticationToken;
+      String username=token.getUsername();
+      String password=new String(token.getPassword());
+
+
+        //创建简单认证信息对象
+        SimpleAuthenticationInfo info=new SimpleAuthenticationInfo(token.getPrincipal(),token.getCredentials(),getName());
+        System.out.println("登录认证");
+        return info;
+    }
+//授权认证
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principal) {
+        String username= principal.getPrimaryPrincipal().toString();
+
+//角色
+        Set<String> roleNameSet=new HashSet<String>();
+        roleNameSet.add("系统管理员");
+        roleNameSet.add("运维");
+//权限
+        Set<String> authenticNameSet=new HashSet<String>();
+        authenticNameSet.add("sys:user:create");
+        authenticNameSet.add("sys:user:update");
+        authenticNameSet.add("sys:user:select");
+        authenticNameSet.add("sys:user:delete");
+
+        //简单授权对象，包括用户角色和权限信息
+        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+        info.addRoles(roleNameSet);
+        info.addStringPermissions(authenticNameSet);
+        System.out.println("授权完成");
+        return info;
+    }
+
+
+}
